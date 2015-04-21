eaecd1a497c6d039cd15aa1033a53211216f5ce7
Wouter1 <W.Pasman@TUDelft.nl>
2015-04-21 13:07:26 +0200

    #3515 removed "-nohomedir" as it destabilizes UT3

--- Installer/src/main/izpack/ut3scripts/startGamebotsCTFServerReflection.bat
+++ Installer/src/main/izpack/ut3scripts/startGamebotsCTFServerReflection.bat
@@ -1,2 +1,2 @@
 cd Binaries
-ut3.exe server CTF-GOAL-Reflection?game=GameBotsUT3.GBCTFGame?bIsDedicated=True?bIsLanMatch=True?bUsesStats=False?TimeLimit=99999 -nohomedir -unattended
+ut3.exe server CTF-GOAL-Reflection?game=GameBotsUT3.GBCTFGame?bIsDedicated=True?bIsLanMatch=True?bUsesStats=False?TimeLimit=99999 -unattended
