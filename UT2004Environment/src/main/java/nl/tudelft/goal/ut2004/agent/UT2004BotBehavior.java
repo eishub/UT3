bea89e1cba79a227e113a743ead70883f56cc45e
Wouter1 <W.Pasman@TUDelft.nl>
2015-04-28 16:56:48 +0200

    #3528 UT3 env returns empty path (instead of exception) when targeting unreachable pathnode

--- UT2004Environment/src/main/java/nl/tudelft/goal/ut2004/agent/UT2004BotBehavior.java
+++ UT2004Environment/src/main/java/nl/tudelft/goal/ut2004/agent/UT2004BotBehavior.java
@@ -743,11 +743,15 @@ public class UT2004BotBehavior extends UT2004BotModuleController<UT2004Bot>
 
 		double distance = sfwMap.getDistance(fromNav, toNav);
 		List<NavPoint> navPoints = sfwMap.getPath(fromNav, toNav);
-		List<UnrealId> unrealIds = new ArrayList<UnrealId>(navPoints.size());
-		for (NavPoint n : navPoints) {
-			unrealIds.add(n.getId());
+		// can be null : locations both exist but no connecting path exists
+
+		List<UnrealId> unrealIds = new ArrayList<UnrealId>();
+		if (navPoints != null) {
+			for (NavPoint n : navPoints) {
+				unrealIds.add(n.getId());
+			}
 		}
-		return new Percept(from, to, distance, unrealIds);
+		return new Percept(fromNav.getId(), toNav.getId(), distance, unrealIds);
 	}
 
 	/**
